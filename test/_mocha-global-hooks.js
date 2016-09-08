cb37a47f195308692d6746d86316bdcc5d388b9a
Judit Ross <jtrs@incowia.com>
2016-09-08 16:11:35 +0200

    eslint with semicolons

--- test/_mocha-global-hooks.js
+++ test/_mocha-global-hooks.js
@@ -7,62 +7,62 @@
  */
 
 /* globals before, after */
-'use strict'
+'use strict';
 var opts = {
   couchUrl: 'http://admin:admin@boot2docker.me:5984',
   dbNamePrefix: 'webpackage-store',
   storeName: 'base-api-upload-test',
   finallyRemoveTestData: process.env.REMOVE_TESTDATA ? JSON.parse(process.env.REMOVE_TESTDATA) : true
-}
-var supercouch = require('supercouch')
-var request = require('superagent')
-var couch = supercouch(opts.couchUrl)
-var testdata = require('./testdata/userdata.js')
-var dbName = opts.dbNamePrefix + '-' + opts.storeName
+};
+var supercouch = require('supercouch');
+var request = require('superagent');
+var couch = supercouch(opts.couchUrl);
+var testdata = require('./testdata/userdata.js');
+var dbName = opts.dbNamePrefix + '-' + opts.storeName;
 
 function removeDb (done, next) {
   couch
     .dbExists(dbName)
     .end(function (err, res) {
       if (err) {
-        return done(err)
+        return done(err);
       }
       // db does exist
       if (res === true) {
-        console.log('Removing database: %s', dbName)
+        console.log('Removing database: %s', dbName);
         couch
           .dbDel(dbName)
           .end(function (err, res) {
             if (err) {
-              return done(err)
+              return done(err);
             }
             if (next) {
-              next(done)
+              next(done);
             } else {
-              done()
+              done();
             }
-          })
+          });
       } else {
         if (next) {
-          next(done)
+          next(done);
         } else {
-          done()
+          done();
         }
       }
-    })
+    });
 }
 
 function addDb (done) {
-  console.log('Creating database: %s', dbName)
+  console.log('Creating database: %s', dbName);
   couch
     .dbAdd(dbName)
     .end(function (err, res) {
       if (err) {
-        console.log('dbAdd failed', err)
-        return done(err)
+        console.log('dbAdd failed', err);
+        return done(err);
       }
-      replicateFromCore(done)
-    })
+      replicateFromCore(done);
+    });
 }
 
 function replicateFromCore (done) {
@@ -72,55 +72,55 @@ function replicateFromCore (done) {
     .send('{"source":"webpackage-store-core","target":"' + dbName + '", "doc_ids":["_design/couchapp-webpackage-validator"]}')
     .end(function (err, res) {
       if (err) {
-        console.log('replication form core failed', err)
-        return done(err)
+        console.log('replication form core failed', err);
+        return done(err);
       }
-      addWebpackageDocument(done)
-    })
+      addWebpackageDocument(done);
+    });
 }
 
 function addWebpackageDocument (done) {
-  var doc = { _id: 'pack@1.0.0', foo: 'bar' }
-  console.log('Creating Webpackage: %s\n', doc._id)
+  var doc = { _id: 'pack@1.0.0', foo: 'bar' };
+  console.log('Creating Webpackage: %s\n', doc._id);
   couch
     .db(dbName)
     .insert(doc)
     .end(function (err, res) {
       if (err) {
-        console.log('document insert failed', err)
-        return done(err)
+        console.log('document insert failed', err);
+        return done(err);
       }
-      done()
-    })
+      done();
+    });
 }
 
 function addDocument (db, doc, next) {
-  doc.created = Date.now()
+  doc.created = Date.now();
   couch
     .db(db)
     .insert(doc)
     .end(function (err, res) {
       if (err) {
-        console.log('Document update for "%s" failed [%s]', doc._id, err.message)
+        console.log('Document update for "%s" failed [%s]', doc._id, err.message);
       }
-      console.log('%s "%s" available.', doc.docType ? doc.docType : 'user', doc._id)
-      next()
-    })
+      console.log('%s "%s" available.', doc.docType ? doc.docType : 'user', doc._id);
+      next();
+    });
 }
 
 before(function (done) {
   // function: create a test user
-  console.log('before ....')
+  console.log('before ....');
 
   // add testuser and test-database
   addDocument('_users', testdata.users.user1, function () {
     addDocument('groups', testdata.groups.group1, function () {
       addDocument('acls', testdata.acls.aclStore1, function () {
-        removeDb(done, addDb)
-      })
-    })
-  })
-})
+        removeDb(done, addDb);
+      });
+    });
+  });
+});
 
 after(function (done) {
   function removeDocument (db, docId, next) {
@@ -129,22 +129,22 @@ after(function (done) {
       .get(docId)
       .end(function (err, res) {
         if (err) {
-          console.log(err)
-          return done(err)
+          console.log(err);
+          return done(err);
         }
         couch
           .db(db)
           .remove(docId, res._rev)
           .end(function (err, res) {
             if (err) {
-              console.log('Remove document "%s" failed!', docId)
-              return done(err)
+              console.log('Remove document "%s" failed!', docId);
+              return done(err);
             } else {
-              console.log('Removed document "%s"', docId)
-              next()
+              console.log('Removed document "%s"', docId);
+              next();
             }
-          })
-      })
+          });
+      });
   }
 
   // remove testuser and test-database
@@ -153,12 +153,12 @@ after(function (done) {
     removeDocument('_users', testdata.users.user1._id, function () {
       removeDocument('groups', testdata.groups.group1._id, function () {
         removeDocument('acls', testdata.acls.aclStore1._id, function () {
-          removeDb(done)
-        })
-      })
-    })
+          removeDb(done);
+        });
+      });
+    });
   } else {
-    done()
+    done();
   }
-})
+});
 
--- test/uploaderSinglePackSpec.js
+++ test/uploaderSinglePackSpec.js
@@ -1,15 +1,15 @@
 /*global require,describe,beforeEach,it*/
-'use strict'
-var assert = require('assert')
-var _ = require('lodash')
-var testdata = require('./testdata/userdata.js')
-var uploader
+'use strict';
+var assert = require('assert');
+var _ = require('lodash');
+var testdata = require('./testdata/userdata.js');
+var uploader;
 
 describe('uploader', function () {
-  var defaults
+  var defaults;
   beforeEach(function () {
-    var Uploader = require('../lib/uploader')
-    uploader = new Uploader()
+    var Uploader = require('../lib/uploader');
+    uploader = new Uploader();
     defaults = {
       'access_credentials': {
         'user': testdata.users.user1.logins.local.login,
@@ -22,127 +22,127 @@ describe('uploader', function () {
         'proxy': ''
       },
       'debug': false
-    }
-  })
+    };
+  });
 
   it('should be defined', function () {
     // console.log(JSON.stringify(webpackageDocument));
-    assert(uploader !== undefined, 'is defined')
-  })
+    assert(uploader !== undefined, 'is defined');
+  });
 
   it('should upload \'uploader-test-package1@0.1.0-SNAPSHOT\'', function (done) {
-    var options = _.merge(defaults, { 'debug': false })
+    var options = _.merge(defaults, { 'debug': false });
 
     function callback (err, success) {
-      console.log('success', success)
-      assert(err === null, 'err = ' + err)
-      done()
+      console.log('success', success);
+      assert(err === null, 'err = ' + err);
+      done();
     }
 
     uploader.uploadSingleWebpackage(options, function (err, success) {
       if (err) {
-        console.log('err: ', err)
-        done(err)
+        console.log('err: ', err);
+        done(err);
       } else {
-        var successObject = JSON.parse(success)
-        assert(successObject.ok === true, 'returns \'ok\'.')
-        assert(successObject.id === 'uploader-test-package1@0.1.0-SNAPSHOT', 'returned ' + successObject.id)
+        var successObject = JSON.parse(success);
+        assert(successObject.ok === true, 'returns \'ok\'.');
+        assert(successObject.id === 'uploader-test-package1@0.1.0-SNAPSHOT', 'returned ' + successObject.id);
 
-        console.log('doc.baseContext: ', JSON.stringify(successObject.baseContext, null, '\t'))
-        assert(successObject.baseContext.uploadInfos.client !== undefined, 'Is client defined?')
-        assert(successObject.baseContext.uploadInfos.user !== undefined, 'Expected user to be defined.')
-        assert(successObject.baseContext.uploadInfos.date !== undefined, 'Expected date to be defined.')
+        console.log('doc.baseContext: ', JSON.stringify(successObject.baseContext, null, '\t'));
+        assert(successObject.baseContext.uploadInfos.client !== undefined, 'Is client defined?');
+        assert(successObject.baseContext.uploadInfos.user !== undefined, 'Expected user to be defined.');
+        assert(successObject.baseContext.uploadInfos.date !== undefined, 'Expected date to be defined.');
 
         // upload it the second time, now we are sure the package already exists
-        uploader.uploadSingleWebpackage(options, callback)
+        uploader.uploadSingleWebpackage(options, callback);
       }
-    })
-  })
+    });
+  });
 
   it('should upload \'uploader-test-package1@1.0.0\'', function (done) {
     var localOptions = {
       'source': 'test/testdata/packages/uploader-test-package2',
       'debug': true
-    }
-    var options = _.merge(defaults, localOptions)
+    };
+    var options = _.merge(defaults, localOptions);
 
     function callback (err, success) {
       // console.log('success: ' + success)
-      assert(err !== undefined, 'err = ' + undefined)
+      assert(err !== undefined, 'err = ' + undefined);
       assert(err.message === 'Final Webpackages are NOT allowed to be overwritten.',
-        'err.message = ' + err.message)
-      done()
+        'err.message = ' + err.message);
+      done();
     }
 
     // upload it the first time, in case the testcase runs the first time againt this database
     uploader.uploadSingleWebpackage(options, function (err, success) {
       if (err) {
-        return done(err)
+        return done(err);
       }
       // upload it the second time, now we are sure the package already exists
-      uploader.uploadSingleWebpackage(options, callback)
-    })
-  })
+      uploader.uploadSingleWebpackage(options, callback);
+    });
+  });
 
   it('should respond with error as the source does not exist', function (done) {
     var localOptions = {
       'source': 'test/testdata/packages/uploader-test-package2-unknown',
       'debug': true
-    }
-    var options = _.merge(defaults, localOptions)
+    };
+    var options = _.merge(defaults, localOptions);
 
     var callback = function (err, success) {
-      assert(err !== undefined)
-      assert(err.message.indexOf('ENOENT') > -1)
-      done()
-    }
-    uploader.uploadSingleWebpackage(options, callback)
-  })
+      assert(err !== undefined);
+      assert(err.message.indexOf('ENOENT') > -1);
+      done();
+    };
+    uploader.uploadSingleWebpackage(options, callback);
+  });
 
   it('should respond with error because of invalid manifestFile', function (done) {
     var localOptions = {
       'source': 'test/testdata/packages/uploader-test-package3-invalidManifestFile',
       'debug': true
-    }
-    var options = _.merge(defaults, localOptions)
+    };
+    var options = _.merge(defaults, localOptions);
 
     var callback = function (err, success) {
       // console.log(err.message)
-      assert(err !== undefined)
-      assert(err.message.indexOf('Validation failed.') > -1)
-      done()
-    }
-    uploader.uploadSingleWebpackage(options, callback)
-  })
+      assert(err !== undefined);
+      assert(err.message.indexOf('Validation failed.') > -1);
+      done();
+    };
+    uploader.uploadSingleWebpackage(options, callback);
+  });
 
   it('should respond with error because of unsupported modelVersion', function (done) {
     var localOptions = {
       'source': 'test/testdata/packages/uploader-test-package4-unsupportedModelVersion',
       'debug': true
-    }
-    var options = _.merge(defaults, localOptions)
+    };
+    var options = _.merge(defaults, localOptions);
 
     var callback = function (err, success) {
       // console.log(err.message)
-      assert(err !== undefined)
-      assert(err.message.indexOf('is unknown or not supported') > -1)
-      done()
-    }
-    uploader.uploadSingleWebpackage(options, callback)
-  })
+      assert(err !== undefined);
+      assert(err.message.indexOf('is unknown or not supported') > -1);
+      done();
+    };
+    uploader.uploadSingleWebpackage(options, callback);
+  });
 
   it('should respond with error because of not exisiting manifestFile', function (done) {
     var localOptions = {
       'source': 'test/testdata/packages/uploader-test-package5-noManifestFile',
       'debug': true
-    }
-    var options = _.merge(defaults, localOptions)
+    };
+    var options = _.merge(defaults, localOptions);
 
     var callback = function (err, success) {
-      assert(err !== undefined)
-      assert(err.message.indexOf('ENOENT') > -1)
-      done()
-    }
-    uploader.uploadSingleWebpackage(options, callback)
-  })
-})
+      assert(err !== undefined);
+      assert(err.message.indexOf('ENOENT') > -1);
+      done();
+    };
+    uploader.uploadSingleWebpackage(options, callback);
+  });
+});
